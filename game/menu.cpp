ce7c985887bcfba7b8629a005989b7a6375dcd55
EnzoGuillouche <enzo.guillouche@algosup.com>
2024-03-22 23:51:18 +0100

    menu update with keyboard

--- game/menu.cpp
+++ game/menu.cpp
@@ -4,6 +4,19 @@
 #include <SFML/Audio.hpp>
 #include "Platformer.h"
 
+
+void updateChoicePosition(int& choicePos, int increment, int minValue, int maxValue) {
+    // Move the choice position by the specified increment/decrement
+    choicePos += increment;
+
+    if (choicePos < minValue) {
+        choicePos = maxValue;
+    } 
+    else if (choicePos > maxValue) {
+        choicePos = minValue;
+    }
+}
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Pixel Showdown", sf::Style::Default);
@@ -28,25 +41,39 @@ int main()
         // handle error
     }
 
+    sf::Text Title("Pixel Showdown", font);
+    Title.setPosition(700, 150);
+    Title.setFillColor(sf::Color::Cyan);
+    Title.setScale(3, 3);
+
+    int playButtonPos = 400;
     sf::Text playButton("Play", font);
-    playButton.setPosition(100, 300);
-    playButton.setFillColor(sf::Color::Red);
+    playButton.setPosition(100, playButtonPos);
+    playButton.setFillColor(sf::Color::White);
     playButton.setScale(2, 2);
     
+    int howToPlayButtonPos = 500;
     sf::Text howToPlayButton("How to play", font);
-    howToPlayButton.setPosition(100, 400);
+    howToPlayButton.setPosition(100, howToPlayButtonPos);
     howToPlayButton.setFillColor(sf::Color::White);
     howToPlayButton.setScale(2, 2);
 
+    int settingsButtonPos = 600;
     sf::Text settingsButton("Settings", font);
-    settingsButton.setPosition(100, 500);
+    settingsButton.setPosition(100, settingsButtonPos);
     settingsButton.setFillColor(sf::Color::White);
     settingsButton.setScale(2, 2);
 
+    int exitButtonPos = 700;
     sf::Text exitButton("Exit", font);
-    exitButton.setPosition(100, 600);
+    exitButton.setPosition(100, exitButtonPos);
     exitButton.setFillColor(sf::Color::White);
     exitButton.setScale(2, 2);
+
+    int choicePos = playButtonPos;
+
+    bool isZPressed = false;
+    bool isSPressed = false;
 
     while (window.isOpen())
     {
@@ -56,26 +83,59 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
 
-            //if (event.type == sf::Event::MouseButtonReleased) {
-                sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)) {
-                    music.stop();
-                    launchGame(window);
-                    return 0;
-                }
-                if (howToPlayButton.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
-                    clickSound.play();
-                }
-                if (settingsButton.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
-                    clickSound.play();
-                }
-                if (exitButton.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
-                    window.close();
-                }
-            //}
+
+            // *** Color Update depending on your choice ***
+            if (choicePos == playButtonPos) {
+                playButton.setFillColor(sf::Color::Red);
+                howToPlayButton.setFillColor(sf::Color::White);
+                settingsButton.setFillColor(sf::Color::White);
+                exitButton.setFillColor(sf::Color::White);
+            }
+            else if (choicePos == howToPlayButtonPos) {
+                playButton.setFillColor(sf::Color::White);
+                howToPlayButton.setFillColor(sf::Color::Red);
+                settingsButton.setFillColor(sf::Color::White);
+                exitButton.setFillColor(sf::Color::White);
+            }
+            else if (choicePos == settingsButtonPos) {
+                playButton.setFillColor(sf::Color::White);
+                howToPlayButton.setFillColor(sf::Color::White);
+                settingsButton.setFillColor(sf::Color::Red);
+                exitButton.setFillColor(sf::Color::White);
+            }
+            else if (choicePos == exitButtonPos) { 
+                playButton.setFillColor(sf::Color::White);
+                howToPlayButton.setFillColor(sf::Color::White);
+                settingsButton.setFillColor(sf::Color::White);
+                exitButton.setFillColor(sf::Color::Red);
+            }
+
+            // Update the choice position
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && !isSPressed) {
+                updateChoicePosition(choicePos, 50, playButtonPos, exitButtonPos);
+            } 
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z) && !isZPressed) {
+                updateChoicePosition(choicePos, -50, playButtonPos, exitButtonPos);
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter) && choicePos == playButtonPos) { // checks if you're on the Play button
+                music.stop();
+                launchGame(window);
+                return 0;
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter) && choicePos == howToPlayButtonPos) { // checks if you're on the How to Play button
+                clickSound.play();
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter) && choicePos == settingsButtonPos) { // checks if you're on the Settings button
+                clickSound.play();
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter) && choicePos == exitButtonPos) { // checks if you're on the Exit button
+                window.close();
+            }
         }
 
         window.clear(sf::Color::Black);
+        window.draw(Title);
         window.draw(playButton);
         window.draw(howToPlayButton);
         window.draw(settingsButton);
